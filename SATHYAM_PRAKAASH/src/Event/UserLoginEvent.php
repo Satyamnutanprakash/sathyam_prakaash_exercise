a1a7759751c0ca528c3792c4e23b346e7d3b2340
Satyam Prakash <satyamprakash25@gmail.com>
2023-05-26 17:09:24 +0530

    custom_events

---
+++ SATHYAM_PRAKAASH/src/Event/UserLoginEvent.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\sathyam_prakaash\Event;
+
+use Drupal\Component\EventDispatcher\Event;
+use Drupal\user\UserInterface;
+
+/**
+ * Event that is called when a user log in.
+ */
+class UserLoginEvent extends Event {
+
+const EVENT_NAME = 'custom_events_user_login';
+
+/**
+   * The user account.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+public $account;
+
+/**
+   * Constructs the object.
+   *
+   * @param \Drupal\user\UserInterface $account
+   *   The account of the user logged in.
+   */
+public function __construct(UserInterface $account) {
+    $this->account = $account;  //will return the user account
+}
+
+}
