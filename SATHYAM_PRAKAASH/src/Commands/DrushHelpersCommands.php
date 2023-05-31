e3a72d2d8651f907de00cefd9f6f63703d906c3b
Satyam Prakash <satyamprakash25@gmail.com>
2023-05-31 22:25:43 +0530

    addded custom drush commands

---
+++ SATHYAM_PRAKAASH/src/Commands/DrushHelpersCommands.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\sathyam_prakaash\Commands;
+
+use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
+use Drush\Commands\DrushCommands;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
+class DrushHelpersCommands extends DrushCommands {
+
+  /**
+   * @var Drupal\Core\Entity\EntityTypeManagerInterface $entityManager
+   *    Entity manager service.
+   */
+
+  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+    $this->entityManager = $entityTypeManager;
+    parent::__construct();
+  }
+
+  /**
+   * Command that returns a list of all active-users.
+   *
+   * @field-labels
+   *  id: User Id
+   *  name: User Name
+   *  email: User Email
+   * @default-fields id,name,email
+   *
+   * @usage drush-helpers:active-users
+   *   Returns all active-users
+   *
+   * @command drush active-users:editor
+   * @aliases role-editor
+   *
+   * @return \Consolidation\OutputFormatters\StructuredData\RowsOfFields
+   */
+  public function blockedUsers() {
+    $users = $this->entityManager->getStorage('user')->loadByProperties(['status' => 1]);
+    $rows = [];
+    foreach ($users as $user) {
+      if ($user->hasRole('editor')) {
+        $rows[] = [
+          'id' => $user->id(),
+          'name' => $user->name->value,
+          'email' => $user->mail->value,
+        ];
+      }
+    }
+    return new RowsOfFields($rows);
+  }
+
+}
