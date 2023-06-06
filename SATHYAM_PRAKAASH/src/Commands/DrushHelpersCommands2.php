8a29b0ef669b44f6105bd643bc536f8f38af0bca
Satyam Prakash <satyamprakash25@gmail.com>
2023-06-06 21:26:14 +0530

    drush command for title

--- SATHYAM_PRAKAASH/src/Commands/DrushHelpersCommands2.php
+++ SATHYAM_PRAKAASH/src/Commands/DrushHelpersCommands2.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\custom_drush\Commands;
+namespace Drupal\sathyam_prakaash\Commands;
 
 use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
 use Drush\Commands\DrushCommands;
@@ -13,40 +13,45 @@ class DrushHelpersCommands extends DrushCommands {
    *    Entity query service.
    */
 
+  protected $entityQuery;
+
+  /**
+   * Constructs a new DrushHelpersCommands object.
+   *
+   * @param Drupal\Core\Entity\EntityTypeManagerInterface $entityQuery
+   *   The entity query service.
+   */
   public function __construct(EntityTypeManagerInterface $entityQuery) {
     $this->entityQuery = $entityQuery;
     parent::__construct();
   }
 
   /**
-   * Command that returns a list of all active-users.
+   * Command that returns a list of all article content titles.
    *
-   * @field-labels
-   *  title: Title
-   *
-   * @usage drush-helpers:title-article
-   *   Returns all the title of articles
-   *
-   * @command drush title-article
+   * @command drush:title-article
    * @aliases title-article
    *
+   * @return \Consolidation\OutputFormatters\StructuredData\RowsOfFields
+   *   The list of article titles.
    */
+  public function getTitleArticle() {
+    $query = $this->entityQuery->getStorage('node')->getQuery(); // Load the Entity Query service.
+    $query->accessCheck(FALSE); // Disable access checks for the query.
+    $entityIds = $query->condition('type', 'article') // Specify the entity type you want to query (e.g., node, user, taxonomy_term).
+                       ->condition('status', 1)  // Add additional conditions if needed.
+                       ->sort('created', 'DESC')
+                       ->range(0, 10)  // Set the range to retrieve only 10 entities.
+                       ->execute(); // Execute the query and get the result.
+    $entities = $this->entityQuery->getStorage('node')->loadMultiple($entityIds); // Load the entities based on the returned IDs.
 
-    public function getTitle() {
-      $query = \Drupal::entityQuery('node'); // Load the Entity Query service.
-      $entityIds = $query->condition('type', 'article') // Specify the entity type you want to query (e.g., node, user, taxonomy_term).
-                         ->condition('status', 1)  // Add additional conditions if needed.
-                         ->sort('created', 'DESC')
-                         ->range(0, 10);  // Set the range to retrieve only 10 entities.
-      $entityIds = $query->execute(); // Execute the query and get the result.
-      $entities = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple($entityIds); // Load the entities based on the returned IDs.
-
-      $titles = [];
-      // Loop through the entities and retrieve their titles.
-      foreach ($entities as $entity) {
-        $title = $entity->label();
-        $titles[] = $title;
-      }
-      return $titles;
+    $titles = [];
+    // Loop through the entities and retrieve their titles.
+    foreach ($entities as $entity) {
+      $title = $entity->label();
+      $titles[] = ['title' => $title];
     }
+
+    return new RowsOfFields($titles);
+  }
 }
