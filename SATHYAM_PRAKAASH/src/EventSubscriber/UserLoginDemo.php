851721fabf04aeb1c324cb5c4aa56803e36c3e18
Satyam Prakash <satyamprakash25@gmail.com>
2023-06-14 18:05:19 +0530

    fixed dependency injection part

--- SATHYAM_PRAKAASH/src/EventSubscriber/UserLoginDemo.php
+++ SATHYAM_PRAKAASH/src/EventSubscriber/UserLoginDemo.php
@@ -4,14 +4,53 @@ namespace Drupal\sathyam_prakaash\EventSubscriber;
 
 // To use the created custom Event.
 use Drupal\sathyam_prakaash\Event\UserLoginEvent;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Class UserLoginSubscriber.
- *
- * @package Drupal\sathyam_prakaash\EventSubscriber
+ * User Login Event Class.
  */
 class UserLoginDemo implements EventSubscriberInterface {
+
+  /**
+   * The database service.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * The date formatter service.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  protected $dateFormatter;
+
+  /**
+   * The messenger service.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * UserLoginDemo constructor.
+   *
+   * @param \Drupal\Core\Database\Connection $database
+   *   The database service.
+   * @param \Drupal\Core\Datetime\DateFormatterInterface $dateFormatter
+   *   The date formatter service.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger service.
+   */
+  public function __construct(Connection $database, DateFormatterInterface $dateFormatter, MessengerInterface $messenger) {
+    $this->database = $database;
+    $this->dateFormatter = $dateFormatter;
+    $this->messenger = $messenger;
+  }
 
   /**
    * {@inheritdoc}
@@ -24,28 +63,36 @@ class UserLoginDemo implements EventSubscriberInterface {
   }
 
   /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiate the class using the container.
+    return new static(
+      $container->get('database'),
+      $container->get('date.formatter'),
+      $container->get('messenger')
+    );
+  }
+
+  /**
    * Subscribe to the user login event dispatched.
    *
    * @param \Drupal\sathyam_prakaash\Event\UserLoginEvent $event
    *   Our custom general object.
    */
   public function onUserLogin(UserLoginEvent $event) {
-    // Using the service of database.
-    $database = \Drupal::database();
-    // Using the service to get date.formatter.
-    $dateFormatter = \Drupal::service('date.formatter');
-
     // Selecting the table from db.
-    $account_created = $database->select('users_field_data', 'ud')
-    // Returns when the account was created.
+    $account_created = $this->database->select('users_field_data', 'ud')
+      // Returns when the account was created.
       ->fields('ud', ['created'])
-    // Returns the userid.
+      // Returns the userid.
       ->condition('ud.uid', $event->account->id())
       ->execute()
       ->fetchField();
+
     // Using message service to get message whenever user logs in.
-    \Drupal::messenger()->addStatus(t('Welcome to the site, your account was created on %created_date.', [
-      '%created_date' => $dateFormatter->format($account_created, 'long'),
+    $this->messenger->addStatus(t('Welcome to the site, your account was created on %created_date.', [
+      '%created_date' => $this->dateFormatter->format($account_created, 'long'),
     ]));
   }
 
--- SATHYAM_PRAKAASH/src/Plugin/Block/SPBlock.php
+++ SATHYAM_PRAKAASH/src/Plugin/Block/SPBlock.php
@@ -2,6 +2,10 @@
 
 namespace Drupal\sathyam_prakaash\Plugin\Block;
 
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Render\RendererInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 use Drupal\Core\Block\BlockBase;
 
 /**
@@ -12,20 +16,67 @@ use Drupal\Core\Block\BlockBase;
  * admin_label = "SP Block"
  * )
  */
-class SPBlock extends BlockBase {
+class SPBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
   /**
-   * Rendering function.
+   * The form builder service.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * The renderer service.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
+   * ExampleBlock constructor.
+   *
+   * @param array $configuration
+   *   The block configuration.
+   * @param string $plugin_id
+   *   The block plugin ID.
+   * @param mixed $plugin_definition
+   *   The block plugin definition.
+   * @param \Drupal\Core\Form\FormBuilderInterface $formBuilder
+   *   The form builder service.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $formBuilder, RendererInterface $renderer) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $formBuilder;
+    $this->renderer = $renderer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder'),
+      $container->get('renderer')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
    */
   public function build() {
     // Create a new instance of your form.
-    $form = \Drupal::formBuilder()->getForm('\Drupal\sathyam_prakaash\Form\ModifiedForm');
+    $form = $this->formBuilder->getForm('\Drupal\sathyam_prakaash\Form\ModifiedForm');
 
     // Render the form using drupal_render().
     $render_array = [
       'form' => $form,
     ];
-    $output = \Drupal::service('renderer')->renderRoot($render_array);
+    $output = $this->renderer->renderRoot($render_array);
     return [
       '#markup' => $output,
     ];
